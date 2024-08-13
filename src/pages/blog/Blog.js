c359dc26b38806659d23b39178b84a0a05f97d0e
Kavan Gondalia <kavan155gondalia@gmail.com>
2024-08-13 20:47:21 +0530

    fix: remove nested css

--- src/pages/blog/Blog.js
+++ src/pages/blog/Blog.js
@@ -69,7 +69,10 @@ function Blog() {
   const totalPages = Math.ceil(blogs.length / itemsPerPage);
 
   // Change page
-  const paginate = (pageNumber) => setCurrentPage(pageNumber);
+  const paginate = (pageNumber) => {
+    setCurrentPage(pageNumber);
+    window.scrollTo(0, 0);
+  };
 
   return (
     <div className="blog">
