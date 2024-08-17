40b917a38a62a6e216ed97d42a5756dddff9928b
Kavan Gondalia <kavan155gondalia@gmail.com>
2024-08-17 14:56:37 +0530

    feat(home): statistics number animation

--- src/pages/home/sections/Statistics.js
+++ src/pages/home/sections/Statistics.js
@@ -1,11 +1,48 @@
+import { useInView } from "react-intersection-observer";
+import AnimatedNumbers from "react-animated-numbers";
+
+const Counter = ({ endValue }) => {
+  const [ref, inView] = useInView({
+    triggerOnce: true,
+    threshold: 0.1,
+    initialInView: true,
+  });
+
+  return (
+    <div ref={ref}>
+      {inView && (
+        <AnimatedNumbers
+          animateToNumber={endValue}
+          transitions={(index) => ({
+            type: "tween",
+            duration: index + 0.3,
+          })}
+        />
+      )}
+    </div>
+  );
+};
+
 const Statitics = () => {
   return (
     <div className="statistics">
       <div className="grid grid-col grid-cols-1 lg:grid-cols-3 gap-4">
-        <StatisticsCard title={"100+"} subtitle={"Projects Secured"} />
-        <StatisticsCard title={"2400+"} subtitle={"Vulnerability detected"} />
         <StatisticsCard
-          title={"$600 Millions+"}
+          unit=""
+          number={100}
+          postNumber={"+"}
+          subtitle={"Projects Secured"}
+        />
+        <StatisticsCard
+          unit=""
+          number={2400}
+          postNumber={"+"}
+          subtitle={"Vulnerability detected"}
+        />
+        <StatisticsCard
+          unit="$"
+          number={"600"}
+          postNumber={" Millions+"}
           subtitle={"Digital Assets Protected"}
         />
       </div>
@@ -13,11 +50,15 @@ const Statitics = () => {
   );
 };
 
-const StatisticsCard = ({ title, subtitle }) => {
+const StatisticsCard = ({ unit = "", number, postNumber, subtitle }) => {
   return (
     <div className="statistics-card">
       <div className="pt-2 text-2xl mg:text-3xl lg:text-4xl font-bold">
-        {title}
+        <div className="flex items-end gap-x-1">
+          {unit}
+          {<Counter endValue={number} />}
+          {postNumber}
+        </div>
       </div>
       <div className="statistics-card-subtitle">{subtitle}</div>
     </div>
