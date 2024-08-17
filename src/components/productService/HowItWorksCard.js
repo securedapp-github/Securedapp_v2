9389843b6325490364f49b0f209f488ce1ccf3dc
Kavan Gondalia <kavan155gondalia@gmail.com>
2024-08-17 22:37:14 +0530

    fix: benefits card alignment

--- src/components/productService/HowItWorksCard.js
+++ src/components/productService/HowItWorksCard.js
@@ -3,14 +3,15 @@ import "./HowItWorksCard.css";
 const HowItWorksCard = ({ image, imageAlt, title, description }) => {
   return (
     <div className="how-it-works-card">
-      <div className="how-it-works-card-image">
-        <img src={image} alt={imageAlt} />
-      </div>
+      {image && (
+        <div className="how-it-works-card-image">
+          <img src={image} alt={imageAlt} />
+        </div>
+      )}
       {title && <div className="how-it-works-card-title">{title}</div>}
       <div
         className="how-it-works-card-description"
-        dangerouslySetInnerHTML={{ __html: description }}
-      ></div>
+        dangerouslySetInnerHTML={{ __html: description }}></div>
     </div>
   );
 };
