3e9e6af8d5a1090426394e89f0cad28161da1bfe
Kavan Gondalia <kavan155gondalia@gmail.com>
2024-08-18 10:26:54 +0530

    feat: start with sidebar for sss

---
+++ src/SolidityShield/components/sidebar/Sidebar.js
@@ -0,0 +1,11 @@
+import "./Sidebar.css";
+
+const Sidebar = () => {
+  return (
+    <div className="sss-sidebar-container">
+      <div className="sss-sidebar"></div>
+    </div>
+  );
+};
+
+export default Sidebar;
