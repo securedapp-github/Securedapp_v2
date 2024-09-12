7936bc4222ec1f6d565592a6b57a206666e566dd
santhoshreddy-dev <santhoshreddy.hello@gmail.com>
2024-09-12 14:41:24 +0530

    add canonical

--- src/components/common/MetaTags.js
+++ src/components/common/MetaTags.js
@@ -131,6 +131,9 @@ const MetaTags = ({ data }) => {
         href="https://fonts.googleapis.com/css2?family=Nunito+Sans:ital,opsz,wght@0,6..12,200..1000;1,6..12,200..1000&family=Nunito:ital,wght@0,200..1000;1,200..1000&family=Outfit:wght@100..900&family=Outfit:wght@100..900&family=Playfair+Display:ital,wght@0,400..900;1,400..900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
         rel="stylesheet"
       />
+
+      <link rel="canonical" href={url} />
+
       <script
         dangerouslySetInnerHTML={{
           __html: `
