4fdec420546de06ac69dc4460592eeb451429fcb
santhoshreddy-dev <santhoshreddy.hello@gmail.com>
2024-09-25 06:42:19 +0530

    fix GA tag

--- src/components/common/MetaTags.js
+++ src/components/common/MetaTags.js
@@ -1,3 +1,5 @@
+"use client";
+
 import { useRouter } from "next/router";
 import Head from "next/head";
 import { GoogleAnalytics } from "@next/third-parties/google";
@@ -66,77 +68,78 @@ const MetaTags = ({ data }) => {
   };
 
   return (
-    <Head>
-      <link
-        rel="stylesheet"
-        href="https://site-assets.fontawesome.com/releases/v6.6.0/css/all.css"
-      />
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <title>{data.title}</title>
-      <meta name="author" content="SecureDapp" />
-      <meta name="theme-color" content="#000000" />
-      <link
-        rel="icon"
-        type="image/x-icon"
-        href="https://securedapp.io/assets/images/logo.png"
-      />
-      <link
-        rel="apple-touch-icon"
-        href="https://securedapp.io/assets/images/logo.png"
-      />
-
-      {/* Basic Meta Tags */}
-      {<meta name="description" content={data.desc} />}
-      {<meta name="keywords" content={data.keywords} />}
-
-      {/* Open Graph Meta Tags (for social media) */}
-      {<meta property="og:title" content={data.title} />}
-      {<meta property="og:description" content={data.desc} />}
-      {data.image && (
-        <meta
-          property="og:image"
-          content={
-            data.image.includes("https://")
-              ? data.image
-              : "https://securedapp.io" + data.image
-          }
+    <html>
+      <head>
+        <link
+          rel="stylesheet"
+          href="https://site-assets.fontawesome.com/releases/v6.6.0/css/all.css"
         />
-      )}
-      {<meta property="og:url" content={url} />}
-      {<meta property="og:type" content="website" />}
-
-      {/* Twitter Meta Tags */}
-      {<meta name="twitter:card" content="summary_large_image" />}
-      {<meta name="twitter:title" content={data.title} />}
-      {<meta name="twitter:description" content={data.desc} />}
-      {data.image && (
-        <meta
-          name="twitter:image"
-          content={
-            data.image.includes("https://")
-              ? data.image
-              : "https://securedapp.io" + data.image
-          }
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>{data.title}</title>
+        <meta name="author" content="SecureDapp" />
+        <meta name="theme-color" content="#000000" />
+        <link
+          rel="icon"
+          type="image/x-icon"
+          href="https://securedapp.io/assets/images/logo.png"
         />
-      )}
+        <link
+          rel="apple-touch-icon"
+          href="https://securedapp.io/assets/images/logo.png"
+        />
 
-      {/* SEO Meta Tags */}
-      {<meta name="robots" content="index, follow" />}
-      {<meta name="googlebot" content="index, follow" />}
+        {/* Basic Meta Tags */}
+        {<meta name="description" content={data.desc} />}
+        {<meta name="keywords" content={data.keywords} />}
 
-      {/* Google Fonts */}
-      <link rel="preconnect" href="https://fonts.googleapis.com" />
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-      <link
-        href="https://fonts.googleapis.com/css2?family=Nunito+Sans:ital,opsz,wght@0,6..12,200..1000;1,6..12,200..1000&family=Nunito:ital,wght@0,200..1000;1,200..1000&family=Outfit:wght@100..900&family=Outfit:wght@100..900&family=Playfair+Display:ital,wght@0,400..900;1,400..900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
-        rel="stylesheet"
-      />
+        {/* Open Graph Meta Tags (for social media) */}
+        {<meta property="og:title" content={data.title} />}
+        {<meta property="og:description" content={data.desc} />}
+        {data.image && (
+          <meta
+            property="og:image"
+            content={
+              data.image.includes("https://")
+                ? data.image
+                : "https://securedapp.io" + data.image
+            }
+          />
+        )}
+        {<meta property="og:url" content={url} />}
+        {<meta property="og:type" content="website" />}
 
-      <link rel="canonical" href={url} />
+        {/* Twitter Meta Tags */}
+        {<meta name="twitter:card" content="summary_large_image" />}
+        {<meta name="twitter:title" content={data.title} />}
+        {<meta name="twitter:description" content={data.desc} />}
+        {data.image && (
+          <meta
+            name="twitter:image"
+            content={
+              data.image.includes("https://")
+                ? data.image
+                : "https://securedapp.io" + data.image
+            }
+          />
+        )}
 
-      <script
-        dangerouslySetInnerHTML={{
-          __html: `
+        {/* SEO Meta Tags */}
+        {<meta name="robots" content="index, follow" />}
+        {<meta name="googlebot" content="index, follow" />}
+
+        {/* Google Fonts */}
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Nunito+Sans:ital,opsz,wght@0,6..12,200..1000;1,6..12,200..1000&family=Nunito:ital,wght@0,200..1000;1,200..1000&family=Outfit:wght@100..900&family=Outfit:wght@100..900&family=Playfair+Display:ital,wght@0,400..900;1,400..900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+          rel="stylesheet"
+        />
+
+        <link rel="canonical" href={url} />
+
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
     window.dataLayer = window.dataLayer || [];
     function gtag() {
       dataLayer.push(arguments);
@@ -144,23 +147,25 @@ const MetaTags = ({ data }) => {
     gtag("js", new Date());
 
     gtag("config", "G-GXZX7PXY8D");`,
-        }}
-      />
-      <GoogleAnalytics gaId="G-GXZX7PXY8D" />
+          }}
+        />
 
-      <script
-        type="application/ld+json"
-        dangerouslySetInnerHTML={{ __html: JSON.stringify(orgSchema) }}
-      />
-      <script
-        type="application/ld+json"
-        dangerouslySetInnerHTML={{ __html: JSON.stringify(webSchema) }}
-      />
-      <script
-        type="application/ld+json"
-        dangerouslySetInnerHTML={{ __html: JSON.stringify(faqSchema) }}
-      />
-    </Head>
+        <GoogleAnalytics gaId="G-GXZX7PXY8D" dataLayerName="ga" />
+
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(orgSchema) }}
+        />
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(webSchema) }}
+        />
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(faqSchema) }}
+        />
+      </head>
+    </html>
   );
 };
 
