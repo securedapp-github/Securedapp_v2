0013b487d1f0431dbc5190aafcb5f2edc2451bde
manicdon7 <manicdon7@gmail.com>
2024-10-08 14:03:46 +0530

    rendering, api need to be integrated

--- src/AuditExpress/components/Hero.tsx
+++ src/AuditExpress/components/Hero.tsx
@@ -199,6 +199,9 @@ const Hero = (props: Props) => {
       }
 
       if (sourceCode) {
+        const lineCount = sourceCode.split('\n').length;
+        console.log(`Number of lines of code: ${lineCount}`);
+
         // Extract compiler version and contract name
         const compilerVersion = extractCompilerVersion(sourceCode);
         let extractedContractName = contractName;
@@ -221,6 +224,7 @@ const Hero = (props: Props) => {
           company_name: companyName,
           contract_name: extractedContractName,
           source_code: sourceCode,
+          lines_of_code: lineCount
         };
         console.log(jsonData);
         
@@ -275,7 +279,7 @@ const Hero = (props: Props) => {
             <div className="lg:flex gap-2 hidden">
               {[...Array(5)].map((_, index) => (
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-8 text-yellow-400">
-                <path fill-rule="evenodd" d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.006 5.404.434c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.434 2.082-5.005Z" clip-rule="evenodd" />
+                <path fillRule="evenodd" d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.006 5.404.434c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.434 2.082-5.005Z" clipRule="evenodd" />
               </svg>
               
               ))}
